c796ff12426efbc125c95b7ae77108930ea07378
andrealangianese <andre.langia.01@gmail.com>
2026-04-14 11:12:10 +0200

    aggiorna e rimuove dal carrello

--- src/App.jsx
+++ src/App.jsx
@@ -15,10 +15,30 @@ export default function app() {
         const giaNelCarrello = addedProducts.find(p => p.name === product.name)
 
         // se non ce lo aggiungiamo
-        if (!giaNelCarrello) {
-            setAddedProducts([...addedProducts, { ...product, quantity: 1 }])
-        }
+        if (giaNelCarrello) {
+            updateProductQuantity(product.name)
+        } else { setAddedProducts([...addedProducts, { ...product, quantity: 1 }]) }
+
+
     }
+    const removeFromCart = (productName) => {
+        // Crea un nuovo array escludendo il prodotto con quel nome
+        const nuovoCarrello = addedProducts.filter(p => p.name !== productName);
+
+        // Aggiorna lo stato
+        setAddedProducts(nuovoCarrello);
+    };
+
+    const updateProductQuantity = (pName) => {
+        setAddedProducts(preCart =>
+            preCart.map(item =>
+                item.name === pName
+                    ? { ...item, quantity: item.quantity + 1 }
+                    : item
+            )
+        )
+    }
+
     return (
         <>
             <h2>miei prodotti</h2>
@@ -33,7 +53,9 @@ export default function app() {
             <h3>questo è il carrello</h3>
             <section>
                 {addedProducts.map((item, i) => (
-                    <p key={i}>{item.name} che costa {item.price.toFixed(2)}€ e nel carrello è presente in quantità {item.quantity}</p>
+                    <p key={i}>{item.name} che costa {item.price.toFixed(2)}€ e nel carrello è presente in quantità {item.quantity}
+                        <button onClick={() => removeFromCart(item.name)}>Rimuovi dal carrello</button>
+                    </p>
                 ))}
             </section>
         </>
